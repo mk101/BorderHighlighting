b5fdf7e993dd0b9efa29380d7caaf44c876cc7de
Maxim Kolesov <pro100maks222@gmail.com>
2022-11-30 15:19:05 +0300

    Add emgu.CV

---
+++ OpenCV/OpenCV.cs
@@ -0,0 +1,25 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+namespace OpenCV;
+
+public class OpenCv
+{
+    public byte[] GenerateImage()
+    {
+        Mat img = new Mat(200, 400, DepthType.Cv8U, 4);
+        img.SetTo(new Bgr(255, 0, 0).MCvScalar);
+        
+        CvInvoke.PutText(
+            img,
+            "Hello world",
+            new System.Drawing.Point(10, 80),
+            FontFace.HersheyComplex,
+            1.0,
+            new Bgr(0, 255, 0).MCvScalar
+        );
+        
+        return img.GetRawData();
+    }
+}
