45ea9c03bba8ecb1a27ec18e395af464442da078
Maxim Kolesov <pro100maks222@gmail.com>
2022-11-30 15:31:43 +0300

    Add bitmap constructor for byte array and fix chanel bug

--- BorderHighlighting/Common/Bitmap.cs
+++ BorderHighlighting/Common/Bitmap.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace BorderHighlighting.Common;
@@ -20,6 +21,21 @@ public class Bitmap
         _writeableBitmap.CopyPixels(_data.Pixels, _data.Stride, 0);
     }
 
+    public Bitmap(byte[] pixels, int width, int height)
+    {
+        _writeableBitmap = new WriteableBitmap(width, height, 96, 96, PixelFormats.Bgr32, null);
+        _data = new BitmapData()
+        {
+            Width = width,
+            Height = height,
+            Stride = 4 * width,
+            Pixels = new byte[4 * width * height]
+        };
+
+        _encoder = new PngBitmapEncoder();
+        Array.Copy(pixels, _data.Pixels, width*height);
+    }
+
     public int Width => _data.Width;
     public int Height => _data.Height;
 
@@ -37,9 +53,9 @@ public class Bitmap
 
         var index = y * _data.Stride + 4 * x;
         return new Color(
-            _data.Pixels[index],
-            _data.Pixels[index + 1],
             _data.Pixels[index + 2],
+            _data.Pixels[index + 1],
+            _data.Pixels[index],
             _data.Pixels[index + 3]
         );
     }
@@ -59,9 +75,9 @@ public class Bitmap
         var index = y * _data.Stride + 4 * x;
         var pixels = _data.Pixels;
         
-        pixels[index] = color.R;
+        pixels[index] = color.B;
         pixels[index + 1] = color.G;
-        pixels[index + 2] = color.B;
+        pixels[index + 2] = color.R;
         pixels[index + 3] = color.A;
     }
 
