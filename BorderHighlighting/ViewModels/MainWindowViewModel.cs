6e3546c2c4850b8c41aa996779eb1c3727bc8ae8
beer2002 <uvarlin77@gmail.com>
2022-12-04 15:37:16 +0300

    Add Cobel and Prewitt without openCV

--- BorderHighlighting/ViewModels/MainWindowViewModel.cs
+++ BorderHighlighting/ViewModels/MainWindowViewModel.cs
@@ -4,6 +4,7 @@ using System.Windows.Media;
 using BorderHighlighting.Common;
 using BorderHighlighting.Common.MVVM;
 using OpenCV;
+using BorderHighlighting.Models;
 
 namespace BorderHighlighting.ViewModels;
 
@@ -47,10 +48,39 @@ public class MainWindowViewModel : NotifyPropertyChanged
             _cvBitmap = new Bitmap(img);
             CvImage = _cvBitmap.GetBitmapSource();
         });
+
+        CobelCommand = new RelayCommand(() =>
+        {
+            var sourceImage = _ourBitmap;
+            if (sourceImage == null)
+            {
+                return;
+            }
+            
+            var image = CobelService.Processing(sourceImage);
+            var resImage = image.GetBitmapSource();
+            OurImage = resImage;
+        });
+        
+        PrewittCommand = new RelayCommand(() =>
+        {
+            var sourceImage = _ourBitmap;
+            if (sourceImage == null)
+            {
+                return;
+            }
+            
+            var image = PrewittService.Processing(sourceImage);
+            var resImage = image.GetBitmapSource();
+            OurImage = resImage;
+        });
     }
     
     public RelayCommand OpenCommand { get; }
     public RelayCommand SaveCommand { get; }
+    
+    public RelayCommand CobelCommand { get; }
+    public RelayCommand PrewittCommand { get; }
     
     public RelayCommand CannyCvCommand { get; }
 
