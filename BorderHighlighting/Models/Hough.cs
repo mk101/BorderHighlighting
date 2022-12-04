d54de5c73bc719dff3bc234b0cea36f4e2ce349a
Maxim Kolesov <pro100maks222@gmail.com>
2022-12-04 15:56:08 +0300

    Add Hough lines

---
+++ BorderHighlighting/Models/Hough.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using BorderHighlighting.Common;
+
+namespace BorderHighlighting.Models;
+
+public class Hough
+{
+    public struct Line
+    {
+        public double? K;
+        public double B;
+        public double X;
+
+        public Line(double? k, double b, double x)
+        {
+            this.K = k;
+            this.B = b;
+            this.X = x;
+        }
+    }
+
+    public List<Line> FindLines(Bitmap cannyImage, int threshold = 100)
+    {
+        const double pi = Math.PI;
+        const int delta = 1;
+
+        int width = cannyImage.Width;
+        int height = cannyImage.Height;
+        int d = (int) (Math.Round(Math.Sqrt(width * width + height * height)) + 1);
+        //int[,] accumulate = new int[cannyImage.Width, cannyImage.Height];
+        
+        int[,] accumulate = new int[2*d + 1, 181];
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                if (cannyImage.GetColor(i, j).I != 255)
+                {
+                    continue;
+                }
+
+                for (int th = 0; th <= 180; th += delta)
+                {
+                    double rad = (pi / 180.0) * th;
+                    var (sin, cos) = Math.SinCos(rad);
+                    int p = (int)Math.Round(i * cos + j * sin);
+                    accumulate[p + d, th] += 1;
+                }
+            }
+        }
+
+        List<Line> lines = new List<Line>();
+
+        for (int p = -d; p <= d; p++)
+        {
+            for (int th = 0; th <= 180; th++)
+            {
+                if (accumulate[d + p, th]  >= threshold)
+                {
+                    double rad = (pi / 180.0) * th;
+                    var (sin, cos) = Math.SinCos(rad);
+                    double x0 = (cos * p);
+                    double y0 = (sin * p);
+
+                    double x1 = (x0 + 1000 * (-sin));
+                    double y1 = (y0 + 1000 * cos);
+                    
+                    double x2 = (x0 - 1000 * (-sin));
+                    double y2 = (y0 - 1000 * cos);
+
+                    double? k = null;
+                    double b = y1;
+                    if (Math.Abs(x1 - x2) > Double.Epsilon)
+                    {
+                        k = (y1 - y2) / (x1 - x2);
+                        b = y1 - (x1 * (y1 - y2)) / (x1 - x2);
+                    }
+
+                    lines.Add(new Line(k, b, x0));
+                }
+            }
+        }
+
+        return lines;
+    }
+}
--- BorderHighlighting/ViewModels/MainWindowViewModel.cs
+++ BorderHighlighting/ViewModels/MainWindowViewModel.cs
@@ -3,7 +3,9 @@ using System.Windows;
 using System.Windows.Media;
 using BorderHighlighting.Common;
 using BorderHighlighting.Common.MVVM;
+using BorderHighlighting.Models;
 using OpenCV;
+using Color = BorderHighlighting.Common.Color;
 
 namespace BorderHighlighting.ViewModels;
 
@@ -12,6 +14,7 @@ public class MainWindowViewModel : NotifyPropertyChanged
     public MainWindowViewModel()
     {
         var fm = new FileManager();
+        var hough = new Hough();
         _cv = new OpenCv();
         
         OpenCommand = new RelayCommand(() =>
@@ -47,12 +50,67 @@ public class MainWindowViewModel : NotifyPropertyChanged
             _cvBitmap = new Bitmap(img);
             CvImage = _cvBitmap.GetBitmapSource();
         });
+
+        HoughLineCommand = new RelayCommand(() =>
+        {
+            if (_ourBitmap is null)
+            {
+                return;
+            }
+
+            var lines = hough.FindLines(_ourBitmap);
+
+            foreach (var line in lines)
+            {
+                if (line.K is null)
+                {
+                    if (line.X < 0 || line.X >= _ourBitmap.Width)
+                    {
+                        continue;
+                    }
+                    for (int i = 0; i < _ourBitmap.Height; i++)
+                    {
+                        _ourBitmap.SetColor((int) line.X, i, new Color(255, 0, 0));
+                    }
+                    
+                    continue;
+                }
+
+                for (int i = 0; i < _ourBitmap.Width; i++)
+                {
+                    int y = (int) ((line.K ?? 0) * i + line.B);
+                    if (y < 0 || y >= _ourBitmap.Height)
+                    {
+                        continue;
+                    }
+                    _ourBitmap.SetColor(i, y, new Color(255, 0, 0));
+                }
+            }
+
+            OurImage = _ourBitmap.GetBitmapSource();
+        });
+
+        HoughLineCvCommand = new RelayCommand(() =>
+        {
+            if (_cvBitmap is null)
+            {
+                return;
+            }
+            
+            var id = ConvertService.BitmapToImageData(_cvBitmap);
+            var img = _cv.HoughLines(id, 200, 180, 100);
+            _cvBitmap = new Bitmap(img);
+            CvImage = _cvBitmap.GetBitmapSource();
+        });
     }
     
     public RelayCommand OpenCommand { get; }
     public RelayCommand SaveCommand { get; }
     
     public RelayCommand CannyCvCommand { get; }
+    
+    public RelayCommand HoughLineCommand { get; }
+    public RelayCommand HoughLineCvCommand { get; }
 
     public ImageSource? OurImage
     {
